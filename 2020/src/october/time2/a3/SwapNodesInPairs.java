9ff6b08d47256376c6f78ccda315f1bbf5e7a64f
wzlove <2585700076@qq.com>
2020-10-14 10:59:16 +0800

    第二遍提交 两两交换链表中的节点

--- 2020/src/october/time2/a3/SwapNodesInPairs.java
+++ 2020/src/october/time2/a3/SwapNodesInPairs.java
@@ -23,17 +23,37 @@ public class SwapNodesInPairs {
 
     /**
      * 非递归写法
+     * 迭代写法
      */
     public ListNode swapPairs(ListNode head) {
-
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode result = new ListNode(-1);
+        result.next = head;
+        ListNode temp = result;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode first = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            first.next = end.next;
+            end.next = first;
+            temp = first;
+        }
+        return result.next;
     }
     /**
      * 递归写法
      */
-    /*public ListNode swapPairs(ListNode head) {
-
-        return null;
+   /* public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode left = head;
+        ListNode right = head.next;
+        left.next = swapPairs(right.next);
+        right.next = left;
+        return right;
     }*/
 
     public static void main(String[] args) {
