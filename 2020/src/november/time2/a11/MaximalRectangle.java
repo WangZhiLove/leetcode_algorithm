ab4dd58220a96eefb26b6855ede697437d6a2fe2
wzlove <2585700076@qq.com>
2020-11-16 11:02:39 +0800

    第二次提交 85. 最大矩形

--- 2020/src/november/time2/a11/MaximalRectangle.java
+++ 2020/src/november/time2/a11/MaximalRectangle.java
@@ -1,5 +1,7 @@
 package november.time2.a11;
 
+import java.util.Stack;
+
 public class MaximalRectangle {
     /**
      * 85. 最大矩形
@@ -37,8 +39,49 @@ public class MaximalRectangle {
      * matrix[i][j] 为 '0' 或 '1'
      */
 
+    /**
+     * 这个得换种思路思考，就是进行题意转化，转化成接雨水题目，只不过这里要计算不是雨水的面积，还是下面实体的面积
+     */
     public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int[][] heights = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0) {
+                    heights[i][j] = matrix[i][j] == '0' ? 0 : 1;
+                } else {
+                    heights[i][j] = matrix[i][j] == '0' ? 0 : heights[i - 1][j] + 1;
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < heights.length; i++) {
+            result = Math.max(result, heightArea(heights[i]));
+        }
+        return result;
+    }
 
-        return 0;
+    private int heightArea(int[] height) {
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        while (index < height.length) {
+            if (stack.isEmpty() || height[index] <= height[stack.peek()]) {
+                stack.push(index ++);
+            } else {
+                int height2 = height[stack.pop()];
+                int width = stack.isEmpty() ? index : index - stack.peek() - 1;
+                result = Math.max(result, height2 * width);
+            }
+        }
+        while (!stack.isEmpty()) {
+            int height2 = height[stack.pop()];
+            int width = stack.isEmpty() ? index : index - stack.peek() - 1;
+            result = Math.max(result, height2 * width);
+        }
+
+        return result;
     }
 }
